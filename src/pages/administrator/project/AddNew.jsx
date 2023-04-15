4633d9404d065008a0727f8fcc5c6cdad18421a4
Master Daniel <engrdanywiss@gmail.com>
2023-04-15 15:08:12 +0100

    Package.json updated with project creation page

--- src/pages/administrator/project/AddNew.jsx
+++ src/pages/administrator/project/AddNew.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import ContentHeader from '../../../components/ContentHeader'
 import FormInput from '../../../components/FormInput'
 import PrimaryButton from '../../../components/PrimaryButton'
@@ -7,9 +7,67 @@ import useAxiosClient from '../../../Hooks/useAxiosClient'
 const AddNew = () => {
 
     const sppCodeRef = useRef()
+    const titleRef = useRef()
+    const categoryRef = useRef()
+    const totalRef = useRef()
+    const stateRef = useRef()
+    const lgaRef = useRef()
+    const [letter, setLetter] = useState('')
+    const awardDateRef = useRef()
+    const amountRef = useRef()
+    const descriptionRef = useRef()
+    const [btnStatus, setBtnStatus] = useState(false)
+
     const axios = useAxiosClient()
     const fetchSPPData = async e => {
         // axiosClient
+    }
+
+    const create = () => {
+        if (sppCodeRef.current.value === '') {
+            window.toastr.error('SPP Code is required')
+        } else if (titleRef.current.value === '') {
+            window.toastr.error('Project Title is required')
+        } else if (categoryRef.current.value === '') {
+            window.toastr.error('Project Category is required')
+        } else if (totalRef.current.value === '') {
+            window.toastr.error('Total is required')
+        } else if (stateRef.current.value === '') {
+            window.toastr.error('State is required')
+        } else if (lgaRef.current.value === '') {
+            window.toastr.error('Local government is required')
+        } else if (letter === '') {
+            window.toastr.error('Award letter is required')
+        } else if (awardDateRef.current.value === '') {
+            window.toastr.error('Award date is required')
+        } else if (amountRef.current.value === '') {
+            window.toastr.error('Amount is required')
+        } else if (descriptionRef.current.value === '') {
+            window.toastr.error('Project description is required')
+        } else {
+            setBtnStatus(true);
+            // setBtnText()
+            const formData = new FormData()
+            formData.append('spp_code', sppCodeRef.current.value)
+            formData.append('project_title', titleRef.current.value)
+            formData.append('project_category', categoryRef.current.value)
+            formData.append('total', totalRef.current.value)
+            formData.append('state', stateRef.current.value)
+            formData.append('lgaRef', lgaRef.current.value)
+            formData.append('letter', letter.current.value)
+            formData.append('award_date', awardDateRef.current.value)
+            formData.append('amount', amountRef.current.value)
+            formData.append('amount', descriptionRef.current.value)
+
+            axios.post('/api/project', formData).then(({ data }) => {
+                setBtnStatus(false);
+                document.querySelector('#project').reset()
+                window.toastr.success(data.message)
+            }).catch(({ response }) => {
+                setBtnStatus(false);
+                window.toastr.error(response.data.message)
+            })
+        }
     }
 
     return (
@@ -19,49 +77,36 @@ const AddNew = () => {
                 <div class="container-fluid">
                     <div class="card">
                         <div className='container'>
-                            <div className='row'>
-                                <FormInput className="col-6 form-group mt-3" label="Enter SPP Code" action={() => fetchSPPData()} ref={sppCodeRef} placeholder="SPP Code" />
+                            <form id="project">
+                            <div className='row' enctype="multi-part/formData">
+                                <FormInput className="col-6 form-group mt-3" label="Enter SPP Code" ref={sppCodeRef} action={() => fetchSPPData()} placeholder="SPP Code" />
                                 <FormInput className="col-6 form-group mt-3" label="SPP Name" readonly={true} placeholder="SPP Name" />
                                 <FormInput className="col-6 form-group mt-3" label="SPP Secretary Name" readonly={true} placeholder="SPP Code" />
                                 <FormInput className="col-6 form-group mt-3" label="SPP Secretary Phone" readonly={true} placeholder="SPP Secretary Phone" />
                                 <FormInput className="col-6 form-group mt-3" label="SPP Secretary Email" readonly={true} placeholder="SPP Secretary Email" />
                                 <FormInput className="col-6 form-group mt-3" label="SPP RC Number" readonly={true} placeholder="SPP RC Number" />
-                                <FormInput className="col-6 form-group mt-3" label="Title" placeholder="Project Title" />
-                                <FormInput className="col-6 form-group mt-3" label="Category" placeholder="Project Category" />
-                                <FormInput className="col-6 form-group mt-3" label="Grand Total" placeholder="Project Grand Total" />
-                                <FormInput className="col-6 form-group mt-3" label="State" placeholder="Enter State" />
-                                <FormInput className="col-6 form-group mt-3" label="LGA" placeholder="Local Government Area" />
+                                <FormInput className="col-6 form-group mt-3" label="Title" ref={titleRef} placeholder="Project Title" />
+                                <FormInput className="col-6 form-group mt-3" label="Category" ref={categoryRef} placeholder="Project Category" />
+                                <FormInput className="col-6 form-group mt-3" label="Grand Total" ref={totalRef} placeholder="Project Grand Total" />
+                                <FormInput className="col-6 form-group mt-3" label="State" ref={stateRef} placeholder="Enter State" />
+                                <FormInput className="col-6 form-group mt-3" label="LGA" ref={lgaRef} placeholder="Local Government Area" />
                                 <div className='form-group mt-3 col-6'>
                                     <label htmlFor='file'>Award Letter</label>
                                     <div className='input-group'>
                                         <div className="custom-file">
-                                            <input className="custom-file-input" type='file' id="file" />
+                                            <input className="custom-file-input" defaultValue={letter} onChange={(e) => setLetter(e.files[0])} type='file' id="file" />
                                             <label class="custom-file-label" htmlFor="file">Upload Award Letter</label>
                                         </div>
                                     </div>
                                 </div>
-                                <FormInput className="col-6 form-group mt-3" label="Date Awarded" placeholder="Date Awarded" />
-                                <FormInput className="col-6 form-group mt-3" label="Funding Amount" placeholder="Amount" type="number" />
+                                <FormInput className="col-6 form-group mt-3" label="Date Awarded" ref={awardDateRef} placeholder="Date Awarded" />
+                                <FormInput className="col-6 form-group mt-3" label="Funding Amount" ref={amountRef} placeholder="Amount" type="number" />
                                 <div className='input-group mt-3'>
-                                    <textarea className='form-control' placeholder='Project Description' rows={10}></textarea>
+                                    <textarea className='form-control' ref={descriptionRef} placeholder='Project Description' rows={10}></textarea>
                                 </div>
                             </div>
-                            <h3 className="mt-2">Project MileStone</h3>
-                            <div className='row'>
-                                <FormInput className="col-6 form-group mt-3" label="Project Phase" placeholder="Phase"/>
-                                <FormInput className="col-6 form-group mt-3" label="Project Duration" placeholder="Duration" type="number" />
-                                <FormInput className="col-6 form-group mt-3" label="Cost of Implementation" placeholder="Implementation Cost" type="number" />
-                                <div className='form-group mt-3 col-6'>
-                                    <label htmlFor='file'>Image</label>
-                                    <div className='input-group'>
-                                        <div className="custom-file">
-                                            <input className="custom-file-input" type='file' id="file" />
-                                            <label class="custom-file-label" htmlFor="file">Image</label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <PrimaryButton type="button" title="Create" className="btn btn-primary btn-block btn-sm mb-3" />
+                            <PrimaryButton type="button" title="Create" onClick={create} className="btn btn-primary btn-block btn-sm mb-3" />
+                            </form>
                         </div>
                     </div>
                 </div>
